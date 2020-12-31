7c4013f63de6a2f9d2768505337686b04bfdae6c
Azusa Okamoto <curiousazunas@gmail.com>
2020-12-31 18:46:06 +0900

    htmlを.phpファイル内で書く

---
+++ sample02.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset='utf-8'>
+        <title>東京オリンピック開催年</title>
+    </head>
+
+    <?php
+        $year1 = '1964年';
+        $year2 = '2020年';
+    ?>
+
+    <body>
+        <div class='contents'>
+            前回の東京オリンピックは、
+            <h1>
+                <?php echo $year1; ?>
+            </h1>
+            です。<br>
+            次の東京オリンピックは、
+            <h1>
+                <?php echo $year2; ?>
+            </h1>
+            です。<br>
+        </div>
+    </body>
+</html>
